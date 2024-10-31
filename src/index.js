033a06c7ef1072b8df5e977c994b87479be5b78b
hwangjunseong <junseong5832@gmail.com>
2024-10-31 19:25:54 +0900

    first

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ const el = document.getElementById('root');
 //3 tell react to take control of that element
 const root = ReactDOM.createRoot(el);
 
-//5 show the component on the screen
+//4 show the component on the screen
 root.render(<App />);
